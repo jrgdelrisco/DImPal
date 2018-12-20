21468cd569a01e4194fa852ad8bd2a5a30c8ec47
jrgdelrisco <jrgdelrisco@gmail.com>
2018-12-20 15:53:06 -0800

    Adding console app for testing.

---
+++ PrototypingConsole/ImageLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using DIPAlgorithms;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp;
+using System.IO;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace PrototypingConsole
+{
+    class ImageLoader : IImageIO
+    {
+        public RawRgbaImage<byte> LoadRawRGBA(string path)
+        {
+            using (var image = Image.Load(path))
+            {
+                byte[] rgbaBytes = MemoryMarshal.AsBytes(image.GetPixelSpan()).ToArray();
+                RawRgbaImage<byte> rawImage = new RawRgbaImage<byte>(rgbaBytes, image.Width, image.Height);
+
+                return rawImage;
+            }
+        }
+
+        public void SaveAsRgbaJpegImage(string path, RawRgbaImage<byte> image)
+        {
+            using (var stream = File.OpenWrite(path))
+            {
+                var newimage = Image.LoadPixelData<Rgba32>(image.Raw, image.Width, image.Height);
+                newimage.SaveAsJpeg(stream);
+            }
+        }
+    }
+}
---
+++ PrototypingConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using DIPAlgorithms;
+using DIPAlgorithms.GrayScale.Enhancement;
+using DIPAlgorithms.GrayScale.Morphology;
+using System;
+
+namespace PrototypingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagePath = "../../../images/";
+
+            string filename = "lion.jpg";
+
+            var loader = new ImageLoader();
+            var rawImage = loader.LoadRawRGBA(imagePath + filename);
+
+            var time = Environment.TickCount;
+            var rawgray = Converter.RgbaToGray(rawImage);
+            rawgray.SharpenMorphology();
+            Console.WriteLine($"filter running time: {Environment.TickCount - time} miliseconds");
+
+            rawImage = Converter.GrayToRGBA(rawgray);
+            loader.SaveAsRgbaJpegImage($"{imagePath}filter_{filename}", rawImage);
+            Console.WriteLine($"total time: {Environment.TickCount - time} miliseconds");
+
+            Console.WriteLine($"Image size: {rawImage.Width} x {rawImage.Height} (width x height)");
+            Console.ReadLine();
+        }
+    }
+}
