e2eb21d85cb4c8c2f8d6bd1146f0e7dc0ccc815b
jrgdelrisco <jrgdelrisco@gmail.com>
2018-12-20 15:43:07 -0800

    Adding interface for IO image files.

---
+++ DIPAlgorithms/IImageIO.cs
@@ -0,0 +1,8 @@
+﻿namespace DIPAlgorithms
+{
+    public interface IImageIO
+    {
+        RawRgbaImage<byte> LoadRawRGBA(string path);
+        void SaveAsRgbaJpegImage(string path, RawRgbaImage<byte> image);
+    }
+}
