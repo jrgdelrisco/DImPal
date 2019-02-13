b5db0c49b6bb0a0b4d4334cb0e1575013e39808d
jrgdelrisco <jrgdelrisco@gmail.com>
2019-02-13 09:18:38 -0800

    Adding command line interface features.

--- PrototypingConsole/Program.cs
+++ PrototypingConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using DIPAlgorithms;
 using DIPAlgorithms.GrayScale.Enhancement;
 using DIPAlgorithms.GrayScale.Morphology;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 
 namespace PrototypingConsole
 {
@@ -9,24 +11,34 @@ namespace PrototypingConsole
     {
         static void Main(string[] args)
         {
-            string imagePath = "../../../images/";
+            var configBuilder = new ConfigurationBuilder()
+                .AddCommandLine(args);
+            var configuration = configBuilder.Build();
 
-            string filename = "lion.jpg";
+            var imagePath = configuration["imagePath"];
+            
+            var fileName = "cheetah.jpg";
+            var directory = Directory.GetCurrentDirectory();
+
+
+            if (File.Exists(imagePath))
+            {
+                fileName = Path.GetFileName(imagePath);
+                directory = Path.GetDirectoryName(imagePath);
+            }
 
             var loader = new ImageLoader();
-            var rawImage = loader.LoadRawRGBA(imagePath + filename);
+            var rawImage = loader.LoadRawRGBA(Path.Combine(directory, fileName));
+            var rawgray = Converter.RgbaToGray(rawImage);
 
             var time = Environment.TickCount;
-            var rawgray = Converter.RgbaToGray(rawImage);
-            rawgray.SharpenMorphology();
-            Console.WriteLine($"filter running time: {Environment.TickCount - time} miliseconds");
+            rawgray.Gradient();
+            Console.WriteLine($"Running time: {Environment.TickCount - time} miliseconds");
 
             rawImage = Converter.GrayToRGBA(rawgray);
-            loader.SaveAsRgbaJpegImage($"{imagePath}filter_{filename}", rawImage);
-            Console.WriteLine($"total time: {Environment.TickCount - time} miliseconds");
+            loader.SaveAsRgbaJpegImage(Path.Combine(directory, $"filter_{fileName}"), rawImage);
 
             Console.WriteLine($"Image size: {rawImage.Width} x {rawImage.Height} (width x height)");
-            Console.ReadLine();
         }
     }
 }
