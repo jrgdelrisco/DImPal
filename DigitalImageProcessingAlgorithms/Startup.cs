5a07e08358f1713c91c7f98338a0880f3dbc3d28
jrgdelrisco <jrgdelrisco@gmail.com>
2019-02-27 20:55:06 -0800

    Setting up new service of filters.

--- DigitalImageProcessingAlgorithms/Startup.cs
+++ DigitalImageProcessingAlgorithms/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DigitalImageProcessingAlgorithms.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +23,7 @@ namespace DigitalImageProcessingAlgorithms
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<IFilterService, FilterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
